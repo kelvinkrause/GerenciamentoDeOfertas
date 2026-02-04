a198ae3c5365a945ffdd0734c8da1d32f646f146
kelvinkrause <kelvinrkrause@outlook.com>
2026-02-04 21:05:12 +0000

    Criado o controller Oferta e seus end-points

---
+++ src/Backend/GerenciamentoDeOfertas.API/Controllers/OfertaController.cs
@@ -0,0 +1,52 @@
+﻿using GerenciamentoDeOfertas.Communication.Requests;
+using GerenciamentoDeOfertas.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciamentoDeOfertas.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OfertaController : ControllerBase
+    {
+        [HttpGet("{id:int}")]
+        public IActionResult ListarOfertaPorIdAsync(int id)
+        {
+            return Ok($"Buscando oferta com ID: {id}");
+        }
+
+        [HttpGet]
+        public IActionResult ListarTodasOfertaAsync()
+        {
+            return Ok($"Buscando todas ofertas.");
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegistradoOfertaJson), StatusCodes.Status201Created)]
+        public IActionResult RegistrarOfertaAsync(
+            [FromBody] RequestRegistrarOfertaJson request)
+        {
+            var response = new ResponseRegistradoOfertaJson
+            {
+                Nome = request.Nome,
+                Descricao = request.Descricao,
+                Preco = request.Preco,
+                DataRegistro = request.DataRegistro
+            };
+
+            return Created(string.Empty, response);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult AtualizarOfertaAsync(int id, [FromBody] RequestAtualizarOfertaJson request)
+        {
+            return Ok("Atualizando oferta.");
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeletarOfertaAsync(int id)
+        {
+            return Ok("Deletando oferta.");
+        }
+    }
+}
---
+++ src/Shared/GerenciamentoDeOfertas.Communication/Requests/RequestRegistrarOfertaJson.cs
@@ -0,0 +1,10 @@
+﻿namespace GerenciamentoDeOfertas.Communication.Requests
+{
+    public class RequestRegistrarOfertaJson
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DataRegistro { get; set; }
+    }
+}
