3661a94f11bfc22a0d4c923943cc8a6a0a15bde6
kelvinkrause <kelvinrkrause@outlook.com>
2026-02-04 22:12:52 +0000

    feat: criado RegistrarOfertaUseCase com metodo Execute

---
+++ src/Backend/GerenciamentoDeOfertas.Application/UseCase/Oferta/RegistrarOfertaUseCase.cs
@@ -0,0 +1,22 @@
+﻿using GerenciamentoDeOfertas.Application.Interfaces;
+using GerenciamentoDeOfertas.Communication.Requests;
+using GerenciamentoDeOfertas.Communication.Responses;
+
+namespace GerenciamentoDeOfertas.Application.UseCase.Oferta
+{
+    public class RegistrarOfertaUseCase : IRegistrarOfertaUseCase
+    {
+        public ResponseRegistradoOfertaJson Execute(RequestRegistrarOfertaJson request)
+        {
+            var response = new ResponseRegistradoOfertaJson
+            {
+                Nome = request.Nome,
+                Descricao = request.Descricao,
+                Preco = request.Preco,
+                DataRegistro = request.DataRegistro
+            };
+
+            return response;
+        }
+    }
+}
