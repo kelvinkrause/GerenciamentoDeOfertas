f23c3d8711654f372fde95d1cd222d72a9c967ef
kelvinkrause <kelvinrkrause@outlook.com>
2026-02-25 23:12:27 +0000

    test: adicionado teste de sucesso para RegistrarOfertaValidator

---
+++ tests/CommonTestUtilities/Requests/RequestRegistrarOfertaJsonBuilder.cs
@@ -0,0 +1,17 @@
+﻿using GerenciamentoDeOfertas.Communication.Requests;
+
+namespace CommonTestUtilities.Requests
+{
+    public static class RequestRegistrarOfertaJsonBuilder
+    {
+        public static RequestRegistrarOfertaJson Build()
+        {
+            return new RequestRegistrarOfertaJson
+            {
+                Nome = "Oferta Teste",
+                Descricao = "Descrição da oferta teste",
+                Preco = 100.00m
+            };
+        }
+    }
+}
---
+++ tests/Validators.Test/UseCase/Oferta/Registrar/RegistrarOfertaValidatorTest.cs
@@ -0,0 +1,24 @@
+﻿using CommonTestUtilities.Requests;
+using FluentAssertions;
+using GerenciamentoDeOfertas.Application.Validator;
+
+namespace Validators.Test.UseCase.Oferta.Registrar
+{
+    public class RegistrarOfertaValidatorTest
+    {
+
+        [Fact]
+        public void Success()
+        {
+            var request = RequestRegistrarOfertaJsonBuilder.Build();
+
+            var validator = new RegistrarOfertaValidator();
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeTrue();
+            result.Errors.Should().BeEmpty();
+
+        }
+    }
+}
