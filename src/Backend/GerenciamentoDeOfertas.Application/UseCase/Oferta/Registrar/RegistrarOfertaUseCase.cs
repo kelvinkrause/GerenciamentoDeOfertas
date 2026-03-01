9db4f36e3c59ed4319251a970dc889a912cc7910
kelvinkrause <kelvinrkrause@outlook.com>
2026-03-01 16:12:09 +0000

    feat: adiciona mapeamento de RegisterOfferRequest para entidade Oferta

--- src/Backend/GerenciamentoDeOfertas.Application/UseCase/Oferta/Registrar/RegistrarOfertaUseCase.cs
+++ src/Backend/GerenciamentoDeOfertas.Application/UseCase/Oferta/Registrar/RegistrarOfertaUseCase.cs
@@ -1,28 +1,35 @@
-﻿using FluentValidation;
+﻿using AutoMapper;
+using FluentValidation;
 using GerenciamentoDeOfertas.Application.Interfaces;
 using GerenciamentoDeOfertas.Communication.Requests;
 using GerenciamentoDeOfertas.Communication.Responses;
+using GerenciamentoDeOfertas.Domain.Entities;
 
 namespace GerenciamentoDeOfertas.Application.UseCase.Oferta.Registrar
 {
     public class RegistrarOfertaUseCase : IRegistrarOfertaUseCase
     {
         private readonly IValidator<RequestRegistrarOfertaJson> _validator;
+        private readonly IMapper _mapper;
         public RegistrarOfertaUseCase(
-            IValidator<RequestRegistrarOfertaJson> validator)
+            IValidator<RequestRegistrarOfertaJson> validator,
+            IMapper mapper)
         {
             _validator = validator;
+            _mapper = mapper;
         }
         public ResponseRegistradoOfertaJson Execute(RequestRegistrarOfertaJson request)
         {
 
             Validate(request);
 
+            var oferta = _mapper.Map<Domain.Entities.Oferta>(request);
+
             var response = new ResponseRegistradoOfertaJson
             {
-                Nome = request.Nome,
-                Descricao = request.Descricao,
-                Valor = request.Valor
+                Nome = oferta.Nome,
+                Descricao = oferta.Descricao,
+                Valor = oferta.Valor
             };
 
             return response;
---
+++ src/Backend/GerenciamentoDeOfertas.Domain/Entities/Oferta.cs
@@ -0,0 +1,11 @@
+﻿namespace GerenciamentoDeOfertas.Domain.Entities
+{
+    public class Oferta
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; } = string.Empty;
+        public string? Descricao { get; set; } = string.Empty;
+        public decimal Valor { get; set; }
+        public DateTime DataRegistro { get; set; } = DateTime.UtcNow;
+    }
+}
