897790282e43155a891e2feface0c9623c55483d
kelvinkrause <kelvinrkrause@outlook.com>
2026-03-04 22:31:17 +0000

    test: implementa testes unitários do caso de uso RegistrarOferta

---
+++ tests/CommonTestUtilities/Repositories/OfertaWriteOnlyRepositoryBuilder.cs
@@ -0,0 +1,16 @@
+﻿using GerenciamentoDeOfertas.Domain.Repositories.Oferta;
+using Moq;
+
+namespace CommonTestUtilities.Repositories
+{
+    public static class OfertaWriteOnlyRepositoryBuilder
+    {
+        public static IOfertaWriteOnlyRepository Build()
+        {
+            var mock = new Mock<IOfertaWriteOnlyRepository>();
+
+            return mock.Object;
+
+        }
+    }
+}
---
+++ tests/UseCases.Test/Oferta/Registrar/RegistrarOfertaUseCaseTest.cs
@@ -0,0 +1,40 @@
+﻿using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using GerenciamentoDeOfertas.Application.UseCase.Oferta.Registrar;
+using GerenciamentoDeOfertas.Application.Validator;
+
+namespace UseCases.Test.Oferta.Registrar
+{
+    public class RegistrarOfertaUseCaseTest
+    {
+        [Fact]
+        public async Task Registrar_Oferta_UseCase_Success()
+        {
+            var request = RequestRegistrarOfertaJsonBuilder.Build();
+
+            var useCase = CreateUseCase();
+            
+            var result = await useCase.Execute(request);
+
+            result.Should().NotBeNull();
+            result.Nome.Should().Be(request.Nome);
+        }
+
+        private RegistrarOfertaUseCase CreateUseCase()
+        {
+            var ofertaWriteOnlyRepositoryBuilder = OfertaWriteOnlyRepositoryBuilder.Build();
+            var validate = new RegistrarOfertaValidator();
+            var unitOfWork = UnitOfWorkBuilder.Build();
+            var mapper = MapperBuilder.Build();
+
+            return new RegistrarOfertaUseCase(
+                ofertaWriteOnlyRepositoryBuilder,
+                validate,
+                unitOfWork,
+                mapper
+            );
+        }
+    }
+}
